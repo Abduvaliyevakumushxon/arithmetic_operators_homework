6e3a8fbe62342ae48acd64f9b7daaf4e9bab0e26
Abduvaliyevakumushxon <shodmonovd1985@gmail.com>
2023-12-07 15:13:33 +0500

    6-vazifa tugadi

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -1,10 +1,10 @@
 #Create a variable called 'number' and assign any value of int to it.
 number=81
 #Divide the number by 2 and assign number.
-number1=81//2
+number=number/2
 #Find the fraction of the number and assign to a variable called 'answer'.
-answer=number1/2
+answer=number%2
 #Print the answer.
 print(answer)
 #Print the number.
-print(number1)
+print(number)
